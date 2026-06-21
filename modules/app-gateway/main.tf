a9f4a7a310f3ec2a5c5a906193fca38f0c5288c5
aswin1015 <aswinaswathysaju@gmail.com>
2026-06-21 16:23:02 +0530

    Add SSL policy configuration to Application Gateway module

--- modules/app-gateway/main.tf
+++ modules/app-gateway/main.tf
@@ -35,6 +35,11 @@ resource "azurerm_application_gateway" "this" {
     capacity = 2
   }
 
+  ssl_policy {
+    policy_type = "Predefined"
+    policy_name = "AppGwSslPolicy20220101"
+  }
+
   gateway_ip_configuration {
     name      = "appgw-ip-config"
     subnet_id = var.subnet_id
